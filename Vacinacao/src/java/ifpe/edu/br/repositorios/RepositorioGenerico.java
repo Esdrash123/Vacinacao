5ef1353d1eb79b3445475b7bff966aa694cbe296
Michael Lucas <maycom.lucas10@hotmail.com>
2017-06-24 19:48:48 -0300

    Criacao das Fachadas

--- Vacinacao/src/java/ifpe/edu/br/repositorios/RepositorioGenerico.java
+++ Vacinacao/src/java/ifpe/edu/br/repositorios/RepositorioGenerico.java
@@ -5,6 +5,12 @@
  */
 package ifpe.edu.br.repositorios;
 
+import ifpe.edu.br.entidades.Animal;
+import ifpe.edu.br.entidades.Aplicacoes;
+import ifpe.edu.br.entidades.Doenca;
+import ifpe.edu.br.entidades.Raca;
+import ifpe.edu.br.entidades.Vacina;
+import ifpe.edu.br.entidades.Veterinario;
 import java.util.List;
 
 /**
@@ -22,5 +28,25 @@ public interface RepositorioGenerico<T, G> {
     public T recuperar(G codigo);
 
     public List<T> recuperarTodos();
+
+    public void inserir(Animal animal);
+
+    public void inserir(Aplicacoes aplicacoes);
+
+    public void inserir(Doenca doenca);
+
+    public void inserir(Vacina vacina);
+
+    public void inserir(Veterinario veterinario);
+
+    public void deletar(Animal animal);
+
+    public void deletar(Aplicacoes aplicacoes);
+
+    public void deletar(Doenca doenca);
+
+    public void deletar(Raca raca);
+
+    public void deletar(Veterinario veterinario);
 
 }
--- Vacinacao/src/java/ifpe/edu/br/repositorios/RepositorioRaca.java
+++ Vacinacao/src/java/ifpe/edu/br/repositorios/RepositorioRaca.java
@@ -5,7 +5,12 @@
  */
 package ifpe.edu.br.repositorios;
 
+import ifpe.edu.br.entidades.Animal;
+import ifpe.edu.br.entidades.Aplicacoes;
+import ifpe.edu.br.entidades.Doenca;
 import ifpe.edu.br.entidades.Raca;
+import ifpe.edu.br.entidades.Vacina;
+import ifpe.edu.br.entidades.Veterinario;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -56,6 +61,56 @@ public class RepositorioRaca implements RepositorioGenerico<Raca, Integer> {
     @Override
     public List<Raca> recuperarTodos() {
         return this.listaRaca;
+    }
+
+    @Override
+    public void inserir(Animal animal) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void inserir(Aplicacoes aplicacoes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void inserir(Doenca doenca) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void inserir(Vacina vacina) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void inserir(Veterinario veterinario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deletar(Animal animal) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deletar(Aplicacoes aplicacoes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deletar(Doenca doenca) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deletar(Raca raca) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deletar(Veterinario veterinario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
