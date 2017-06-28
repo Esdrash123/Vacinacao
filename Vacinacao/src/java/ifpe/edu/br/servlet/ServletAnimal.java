0cee9bfe395c37028cb1032263fdc287769d9355
Esdras Henrique <esdrasha1@gmail.com>
2017-06-28 14:05:19 -0300

    Correcao de erros nos servlets

--- Vacinacao/src/java/ifpe/edu/br/servlet/ServletAnimal.java
+++ Vacinacao/src/java/ifpe/edu/br/servlet/ServletAnimal.java
@@ -10,6 +10,8 @@ import ifpe.edu.br.entidades.Animal;
 import ifpe.edu.br.builder.BuilderAnimal;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,48 +19,79 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 /**
  *
  * @author paulo
  */
 @WebServlet(name = "AnimalServlet", urlPatterns = {"/AnimalServlet"})
 public class ServletAnimal extends HttpServlet {
-    /** 
-    private int codigo;
-    private String Nome;
-    private String CNPJ;
-    private String OAB;
-    private String Telefone;
-    private String Endereco;
-    */
+
+    /**
+     * private int codigo; private String Nome; private String CNPJ; private
+     * String OAB; private String Telefone; private String Endereco;
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, ParseException {
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
         String Nome = request.getParameter("Nome");
         int id = Integer.parseInt(request.getParameter("id"));
         String caracteristicaIndividuais = request.getParameter("caracteristicaIndividuais");
-        Date nascimento = request.getParameter("nascimento");
-        double peso = request.getParameter("peso");
-      
-        
+        Date nascimento = formato.parse(request.getParameter("nascimento"));
+        double peso = Integer.parseInt(request.getParameter("peso"));;
+
         BuilderAnimal bAnimal = new BuilderAnimal();
         bAnimal.setId(id);
         bAnimal.setNome(Nome);
         bAnimal.setCaracteristicaIndividuais(caracteristicaIndividuais);
         bAnimal.setNascimento(nascimento);
         bAnimal.setPeso(peso);
-     
-         
+
         Animal animal = bAnimal.BuilderAnimal();
-        
+
         Fachada.getInstance().inserir(animal);
 
-        request.setAttribute("msg","Animal Cadastrado com sucesso!");
+        request.setAttribute("msg", "Animal Cadastrado com sucesso!");
         getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
-    
-}
 
-    private Object getServletContext() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-}
 
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
--- Vacinacao/src/java/ifpe/edu/br/servlet/ServletAplicacoes.java
+++ Vacinacao/src/java/ifpe/edu/br/servlet/ServletAplicacoes.java
@@ -9,66 +9,96 @@
  */
 package ifpe.edu.br.servlet;
 
-import ifpe.edu.br.fachada.Fachada;
+import java.util.Date;
 import ifpe.edu.br.entidades.Aplicacoes;
 import ifpe.edu.br.builder.BuilderAplicacoes;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 /**
  *
  * @author paulo
  */
 @WebServlet(name = "AplicacoesServlet", urlPatterns = {"/AplicacoesServlet"})
 public class ServletAplicacoes extends HttpServlet {
-    /** 
-    private int codigo;
-    private String Nome;
-    private String CNPJ;
-    private String OAB;
-    private String Telefone;
-    private String Endereco;
-    */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-       
-   
 
-        
+    /**
+     * private int codigo; private String Nome; private String CNPJ; private
+     * String OAB; private String Telefone; private String Endereco;
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ParseException {
+
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
         String descricao = request.getParameter("descricao");
         int id = Integer.parseInt(request.getParameter("id"));
-        int hora = Integer.parseInt(request.getParameter("hira"));
-        Date data = request.getParameter("data");
-                Date dataProxima = request.getParameter("dataProxima");
+        int hora = Integer.parseInt(request.getParameter("hora"));
+        Date data = formato.parse(request.getParameter("data"));
+        Date dataProxima = formato.parse(request.getParameter("dataProxima"));
 
-      
-        
         BuilderAplicacoes bAplicacoes = new BuilderAplicacoes();
         bAplicacoes.setId(id);
         bAplicacoes.setDescricao(descricao);
         bAplicacoes.setHora(hora);
         bAplicacoes.setData(data);
-  
-     
-         
+        bAplicacoes.setDataProxima(dataProxima);
+
         Aplicacoes aplicacoes = bAplicacoes.BuilderAplicacoes();
-        
+
         Fachada.getInstance().inserir(aplicacoes);
 
-        request.setAttribute("msg","Aplicacoes Cadastrado com sucesso!");
+        request.setAttribute("msg", "Aplicacoes Cadastrado com sucesso!");
         getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
-    
-}
 
-    private Object getServletContext() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
-
-
