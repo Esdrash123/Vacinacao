e327f19be2ceba1652dcf363c4500463c76e256b
Rochel Angelo <rochelangelo72@gmail.com>
2017-06-28 14:55:30 -0300

    Implementação do servlet Doença

--- Vacinacao/src/java/ifpe/edu/br/servlet/ServletDoenca.java
+++ Vacinacao/src/java/ifpe/edu/br/servlet/ServletDoenca.java
@@ -3,12 +3,106 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package ifpe.edu.br.servlet;
+
+import java.util.Date;
+import ifpe.edu.br.entidades.Aplicacoes;
+import ifpe.edu.br.builder.BuilderAplicacoes;
+import ifpe.edu.br.builder.BuilderDoenca;
+import ifpe.edu.br.entidades.Doenca;
+import ifpe.edu.br.fachada.Fachada;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author paulo
  */
-public class ServletDoenca {
-    
+@WebServlet(name = "ServletDoenca", urlPatterns = {"/ServletDoenca"})
+public class ServletDoenca extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ParseException {
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        String nome = request.getParameter("nome");
+        String descricao = request.getParameter("descricao");
+        
+        
+        BuilderDoenca bDoenca = new BuilderDoenca(id, nome, descricao);
+        bDoenca.setId(id);
+        bDoenca.setNome(nome);
+        bDoenca.setDescricao(descricao);
+
+        Doenca doenca = bDoenca.BuilderDoenca();
+
+        Fachada.getInstance().inserir(doenca);
+
+        request.setAttribute("msg", "Doença Cadastrado com sucesso!");
+        getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+
+    }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
